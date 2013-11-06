5389466829c2276eb4de23d298f6c34e713c87cb
Alexey Melnikov <individum05@gmail.com>
2013-11-06 21:54:36 +0700

    Fix print_counter

--- sc/print_counter.c
+++ sc/print_counter.c
@@ -2,13 +2,6 @@
 
 void print_counter()
 {
-	int command = (sc_memory[inst_counter] >> 14) & 1;
-	int mem = sc_memory[inst_counter] & 0x3FFF;
-
-	mt_gotoXY(69, 5);
-	if (command == 0)
-		printf("+");
-	else
-		printf("-");
-	printf("%0*X", 4, mem);
+	mt_gotoXY(70, 5);
+	printf("%0*X", 4, inst_counter);
 }
